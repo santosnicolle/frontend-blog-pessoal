453f86853840fdfc94b4b5b8d50db982123b2364
Santos <santos.nicolle@aluno.ufabc.edu.br>
2022-04-28 23:36:09 -0300

    subindo atualizações de login e app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import {Grid, Paper} from '@material-ui/core';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import Home from './paginas/home/Home'
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
+import Login from './paginas/login/Login';
 import './App.css';
 
 function App() {
   return (
-    <>
-        <Navbar />
-        <Home />
-        <Footer />
-    </>
-    );
+            <Router>
+                <Navbar />
+                  <div style={{ minHeight: '100vh' }}>
+                    <Routes> {/* Antigo Switch */}
+                      <Route path="/" element={<Login />} />
+                      <Route path="/login" element={<Login />} />
+                      <Route path="/home" element={<Home />} />
+                      <Route path="/cadastro" element={<CadastroUsuario />} />
+                    </Routes>
+                  </div>
+                <Footer />
+            </Router>
+        );
 }
 
 export default App;
--- src/components/estaticos/footer/Footer.tsx
+++ src/components/estaticos/footer/Footer.tsx
@@ -1,37 +1,39 @@
 import React from 'react';
 import InstagramIcon from '@material-ui/icons/Instagram';
-import FacebookIcon from '@material-ui/icons/Facebook';
+import GitHubIcon from "@material-ui/icons/GitHub"
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import {Typography, Box, Grid } from '@material-ui/core';
+
+import './Footer.css';
 
 function Footer () {
     return (
         <>
             <Grid container direction="row" justifyContent="center" alignItems="center">
                 <Grid alignItems="center" item xs={12}>
-                    <Box style={{ backgroundColor: "#3F51B5", height: "120px" }}>
+                    <Box className='box1'>
                         <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Siga-nos nas redes sociais </Typography>
+                            <Typography variant="h6" align="center" className='textos'>Siga-me nas redes sociais </Typography>
                         </Box>
                         <Box display="flex" alignItems="center" justifyContent="center">
-                            <a href="https://www.facebook.com/" target="_blank">
-                                <FacebookIcon style={{ fontSize: 60, color: "white" }} />
+                            <a href="https://www.github.com/santosnicolle" target="_blank">
+                            <GitHubIcon className='redes' />
                             </a>
                             <a href="https://www.instagram.com/stringsantos" target="_blank">
-                                <InstagramIcon style={{ fontSize: 60, color: "white" }} />
+                                <InstagramIcon className='redes' />
                             </a>
                             <a href="https://www.linkedin.com/in/santosnicolle" target="_blank">
-                                <LinkedInIcon style={{ fontSize: 60, color: "white" }} />
+                                <LinkedInIcon className='redes' />
                             </a>
                         </Box>
                     </Box>
-                    <Box style={{ backgroundColor: "#303F9F", height: "60px" }}>
+                    <Box className='box2'>
                         <Box paddingTop={1}>
-                            <Typography variant="subtitle2" align="center" gutterBottom style={{ color: "white" }} >© 2020 Copyright:</Typography>
+                            <Typography variant="subtitle2" align="center" className='textos' >© 2020 Copyright:</Typography>
                         </Box>
                         <Box>
                             <a target="_blank" href="https://brasil.generation.org">
-                                <Typography variant="subtitle2" gutterBottom style={{ color: "white" }} align="center">brasil.generation.org</Typography>
+                                <Typography variant="subtitle2" className='textos' align="center">brasil.generation.org</Typography>
                             </a>
                         </Box>
                     </Box>
